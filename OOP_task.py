547dbcd8406310af431ab6751a5dd20fa5360b53
GennadyMukchamedchanov <dungreshmen2@mail.ru>
2023-11-12 18:30:55 +0300

    ADD DiscountedProduct

--- OOP_task.py
+++ OOP_task.py
@@ -6,6 +6,15 @@ class Product:
     def create(cls, name, price):
         return cls(name, price)
 
-obj = Product.create('aple', '100')
-print(obj.name)
-print(obj.price)
+class DiscountedProduct(Product):
+    def __init__(self, name, price, discount):
+        super().__init__(name, price)
+        self.discount = discount
+
+    def get_discounted_price(self):
+        return self.price - (self.price//100*self.discount)
+
+obj = DiscountedProduct('aple', 100, 5)
+print(obj.get_discounted_price())
+
+
